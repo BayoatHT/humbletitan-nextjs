5f9d01e10938df6f99a8118e23974463c990ecc6
BayoaHT <humbletitanus@gmail.com>
2022-08-17 20:07:43 +0500

    link changed

--- pages/politics/election-center/[index].jsx
+++ pages/politics/election-center/[index].jsx
@@ -179,7 +179,7 @@ export async function getServerSideProps(context) {
   const arr = [];
 
   await axios
-    .get("https://h-t.vercel.app/api/stateelections")
+    .get("https://humbletitan-nextjs.vercel.app/api/stateelections")
     .then((result) => {
       result.data.values.map((item) => {
         arr.push({
